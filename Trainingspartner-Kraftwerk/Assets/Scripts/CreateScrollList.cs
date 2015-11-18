add5d664c10c4f74073f4e68d3f76b0fd1c5ec7f
LudusExtremus <ludusExtremus@gmail.com>
2015-11-18 13:38:40 +0100

    Integrated 2 scripts, which fill the content view panel dynamically with items - Still to do: Give objects, which contains text, icons etc. from parse, to these scripts.

---
+++ Trainingspartner-Kraftwerk/Assets/Scripts/CreateScrollList.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+using System.Collections;
+using System.Collections.Generic;
+
+[System.Serializable]
+public class Item {
+    
+    public Sprite icon;
+    public Sprite buttonImageTop;
+    public Sprite buttonImageMid;
+    public Sprite buttonImageBot;
+    public string textTop;
+    public string textMid;
+    public string textBot;
+    public bool isChampion;
+
+}
+public class CreateScrollList : MonoBehaviour {
+
+    public GameObject newsEntry;
+    public List<Item> itemList;
+
+    public Transform contentPanel;
+
+	void Start () {
+        PopulateList();
+	}
+
+    void PopulateList() {
+        foreach (var item in itemList)
+        {
+            GameObject newNewsEntry = Instantiate(newsEntry) as GameObject;
+            NewsEntryButton newsEntryButton = newNewsEntry.GetComponent<NewsEntryButton>();
+            newsEntryButton.newsIcon.sprite = item.icon;
+            newsEntryButton.buttonImageTop.sprite = item.buttonImageTop;
+            newsEntryButton.buttonImageMid.sprite = item.buttonImageMid;
+            newsEntryButton.buttonImageBot.sprite = item.buttonImageBot;
+            newsEntryButton.textTop.text = item.textTop;
+            newsEntryButton.textMid.text = item.textMid;
+            newsEntryButton.textBot.text = item.textBot;
+            newsEntryButton.championIcon.SetActive(item.isChampion);
+            newNewsEntry.transform.SetParent(contentPanel);
+        }
+
+    }
+}
---
+++ Trainingspartner-Kraftwerk/Assets/Scripts/NewsEntryButton.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+using System.Collections;
+
+public class NewsEntryButton : MonoBehaviour {
+
+    public Image button;
+    public Image newsIcon;
+    public Image buttonImageTop;
+    public Image buttonImageMid;
+    public Image buttonImageBot;
+    public Text textTop;
+    public Text textMid;
+    public Text textBot;
+    public GameObject championIcon;
+}
