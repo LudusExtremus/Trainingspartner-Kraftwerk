7fc9d7d531a5fcffc1346293784c60b59c234acf
Sebastian Drees <sebdrees@gmail.com>
2015-11-20 14:40:11 +0100

    EventManager

---
+++ Trainingspartner-Kraftwerk/Assets/Scripts/EventManager.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using Parse;
+using System.Collections.Generic;
+
+public class EventManager : MonoBehaviour {
+
+    public delegate void UserQueryFinished(List<ParseUser> users);
+    public static event UserQueryFinished onUserQueryFinished;
+
+    public static void triggerUserQueryFinished(List<ParseUser> users)
+    {
+        onUserQueryFinished(users);
+    }
+
+}
