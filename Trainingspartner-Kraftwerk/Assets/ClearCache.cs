90d8a1d692cc28a541c63a1f5058c5aa1e7bc131
LudusExtremus <ludusExtremus@gmail.com>
2016-01-03 14:57:37 +0100

    added icons

---
+++ Trainingspartner-Kraftwerk/Assets/ClearCache.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ClearCache : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+
+        Caching.CleanCache();
+        Debug.Log("Cleared cache most likely");
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
